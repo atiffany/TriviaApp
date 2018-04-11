988886bbc408af27937bf46f3850ea8017530e80
atiffany <abby.tiffany@gmail.com>
2018-04-10 23:18:49 -0700

    cleaning up code with help

--- app/src/main/java/com/abbytiffany/quiz/MainActivity.java
+++ app/src/main/java/com/abbytiffany/quiz/MainActivity.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
-    int total = 6;
+    int total = 5;
 
 
     @Override
@@ -55,36 +55,30 @@ public class MainActivity extends AppCompatActivity {
         score++;
     }
 
-    public void onRadioButtonClicked(int resource) {
-        RadioButton radio = findViewById(resource);
-        boolean checked = radio.isChecked();
+    public boolean onMergedButtonClicked(int resource) {
+        CompoundButton check = findViewById(resource);
+        boolean checked = check.isChecked();
+        boolean flag = false;
         switch (resource) {
+            case R.id.correct_answer_2_a:
+            case R.id.correct_answer_2_b:
+                CheckBox wrongAnswer = findViewById(R.id.answer_2_c);
+                if (checked && !wrongAnswer.isChecked()) {
+                    flag = true;
+
+                }
+                break;
             case R.id.correct_answer_1:
             case R.id.correct_answer_4:
                 if (checked) {
                     updateScore();
-                    break;
-                }
-                else break;
-            default:
-                break;
-        }
-    }
 
-    public void onCheckBoxClicked(int resource) {
-        CheckBox check = findViewById(resource);
-        boolean checked = check.isChecked();
-        switch (resource) {
-            case R.id.correct_answer_2_a:
-            case R.id.correct_answer_2_b:
-                if (checked) {
-                    updateScore();
-                    break;
                 }
-                else break;
+                break;
             default:
                 break;
         }
+        return flag;
     }
 
     public void checkIfTextIsCorrect(int resource) {
@@ -112,11 +106,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void displayFinalScore(View view) {
-        onRadioButtonClicked(R.id.correct_answer_1);
-        onCheckBoxClicked(R.id.correct_answer_2_a);
-        onCheckBoxClicked(R.id.correct_answer_2_b);
+        onMergedButtonClicked(R.id.correct_answer_1);
+        if (onMergedButtonClicked(R.id.correct_answer_2_a) && onMergedButtonClicked(R.id.correct_answer_2_b)){
+            updateScore();
+        }
         checkIfTextIsCorrect(R.id.answer_3);
-        onRadioButtonClicked(R.id.correct_answer_4);
+        onMergedButtonClicked(R.id.correct_answer_4);
         checkIfSpinnerIsCorrect(R.id.question_5_spinner);
 
         String answer = getString(R.string.your_score_is) + " : " + score + "/" + total;
